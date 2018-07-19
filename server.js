77129d15ecf9cb3f9d262dabb8dc811512df4f07
NIthin Antony <nithinparathara007@gmail.com>
2018-07-19 21:58:58 +0530

    update

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-/Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/busmatesweb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/busmatesweb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
