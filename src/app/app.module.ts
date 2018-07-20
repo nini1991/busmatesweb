bf4476a600df4b2ab21fcb3956e6f91f28cbd679
NIthin Antony <nithinparathara007@gmail.com>
2018-07-20 23:30:47 +0530

    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent],
   schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
+    CUSTOM_ELEMENTS_SCHEMA
 ]
 })
 export class AppModule { }
